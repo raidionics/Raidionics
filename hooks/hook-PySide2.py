b4e2a87f6fe73c9dbbd505bee64f222fd0afa0b3
andreped <andrped94@gmail.com>
2021-04-21 16:01:17 +0200

    Had to adjust build settings for pyinstaller to account for us swapping PyQt5 with PySide2

--- hooks/hook-PySide2.py
+++ hooks/hook-PySide2.py
@@ -1,6 +1,6 @@
 from PyInstaller.utils.hooks import collect_submodules
 from PyInstaller.utils.hooks import collect_data_files
 
-hiddenimports = collect_submodules("PyQt5")
+hiddenimports = collect_submodules("PySide2")
 
-datas = collect_data_files("PyQt5")
+datas = collect_data_files("PySide2")
--- main.py
+++ main.py
@@ -3,6 +3,12 @@ import os
 os.environ['LC_CTYPE'] = "en_US.UTF-8"
 os.environ['LANG'] = "en_US.UTF-8"
 
+# relevant for PySide, Qt stuff. See issue here: https://www.programmersought.com/article/8605863159/
+import PySide2
+dirname = os.path.dirname(PySide2.__file__)
+plugin_path = os.path.join(dirname, 'plugins', 'platforms')
+os.environ['QT_QPA_PLATFORM_PLUGIN_PATH'] = plugin_path
+
 import sys
 from PySide2.QtWidgets import QApplication
 # from main_gui_alternative import MainWindow
