7bdf8ce6cc647037c7a5613218f7f89387d7762c
dbouget <bougetd@gmail.com>
2022-01-19 16:21:07 +0100

    Cloud synchronization and download of the models, with caching.

--- segmentation/main.py
+++ segmentation/main.py
@@ -1,5 +1,6 @@
 import os
 import tensorflow as tf
+from utils.models_download import download_model
 from segmentation.src.fit import predict
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
@@ -8,6 +9,7 @@ def main_segmentation(input_filename, output_folder, model_name, brain_mask_file
     os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
     os.environ["CUDA_VISIBLE_DEVICES"] = gpu_device
 
+    download_model(model_name=model_name)
     predict(input_filename=input_filename, output_path=output_folder, selected_model=model_name,
             brain_mask_filename=brain_mask_filepath)
     tf.keras.backend.clear_session()
---
+++ utils/models_download.py
@@ -0,0 +1,45 @@
+import os
+import gdown
+import traceback
+import pandas as pd
+
+
+def get_available_cloud_models_list():
+    cloud_models_list = []
+    cloud_models_list_url = 'https://drive.google.com/uc?id=1vRUr0VXgnDFNq7AlB5ILyBCmW_sGuciP'
+    try:
+        cloud_models_list_filename = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../resources', 'models',
+                                                  'cloud_models_list.csv')
+        gdown.cached_download(url=cloud_models_list_url, path=cloud_models_list_filename,
+                              md5='1545f2cab1df9a6d3fb561c5e497b89f')
+        cloud_models_list = pd.read_csv(cloud_models_list_filename)
+    except Exception as e:
+        print('Impossible to access the cloud models list.\n')
+        print('{}'.format(traceback.format_exc()))
+
+    return cloud_models_list
+
+
+def download_model(model_name):
+    cloud_models_list = get_available_cloud_models_list()
+    if model_name in list(cloud_models_list['Model'].values):
+        model_params = cloud_models_list.loc[cloud_models_list['Model'] == model_name]
+        url = model_params['link'].values[0]
+        md5 = model_params['sum'].values[0]
+        dep = model_params['dependencies'].values[0]
+        models_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../resources', 'models')
+        os.makedirs(models_path, exist_ok=True)
+        models_archive_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), '../resources', 'models',
+                                           '.cache', model_name + '.zip')
+        os.makedirs(os.path.dirname(models_archive_path), exist_ok=True)
+        gdown.cached_download(url=url, path=models_archive_path, md5=md5)
+        gdown.extractall(path=models_archive_path, to=models_path)
+        # os.remove(models_archive_path)
+
+        if dep != dep:
+            return
+
+        for d in dep:
+            download_model(d)
+    else:
+        print("No model exists with the provided name.\n")
