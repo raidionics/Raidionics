30bf263c88ab9ec7dd86f84ebd92afababfdc94b
david bouget <bougetd@gmail.com>
2022-03-29 20:36:44 +0200

    Adjusted overlaid lines in the viewer.

--- gui2/SinglePatientComponent/CustomQOpenGLWidget.py
+++ gui2/SinglePatientComponent/CustomQOpenGLWidget.py
@@ -1,10 +1,10 @@
 from PySide2.QtWidgets import QWidget, QOpenGLWidget, QGraphicsView, QGraphicsScene, QGraphicsPixmapItem
 from PySide2.QtGui import QPixmap, QImage, QPainter, QPen, QColor
-from PySide2.QtCore import Qt, QSize
+from PySide2.QtCore import Qt, QSize, Signal
 from PySide2 import QtOpenGL
 import numpy as np
 
-from OpenGL.GL import *
+# from OpenGL.GL import *
 import nibabel as nib
 
 
@@ -13,26 +13,35 @@ class CustomQOpenGLWidget(QGraphicsView):
     """
 
     """
+    coordinates_changed = Signal(int, int)
+
     def __init__(self, parent=None):
         super(CustomQOpenGLWidget, self).__init__()
         self.parent = parent
 
-        image_nib = nib.load("/media/dbouget/ihdb/Studies/Neuro/NeuroRADS/Article-Q1-2022/200_MR_T1_pre_311_typical_meningioma.nii.gz")
-        image = image_nib.get_data()[:]
-        data = np.ascontiguousarray(np.stack((image[:,:,150], image[:,:,150], image[:,:,150]), axis=0).transpose((1,2,0))).astype(("uint8"))
-        # data = np.zeros((250, 250, 3), dtype="uint8") #image[:, :, 150]
+        # image_nib = nib.load("/media/dbouget/ihdb/Studies/Neuro/NeuroRADS/Article-Q1-2022/200_MR_T1_pre_311_typical_meningioma.nii.gz")
+        # image = image_nib.get_data()[:]
+        # data = np.ascontiguousarray(np.stack((image[:,:,150], image[:,:,150], image[:,:,150]), axis=0).transpose((1,2,0))).astype(("uint8"))
+        data = np.zeros((250, 250, 3), dtype="uint8") #image[:, :, 150]
         qimage = QImage(data, data.shape[1], data.shape[0], QImage.Format_RGB888)
-        pixm = QPixmap(qimage)
-        pixm = pixm.scaled(QSize(int(1140 / 2), int(850 / 2)), Qt.KeepAspectRatio)
+        self.pixmap = QPixmap(qimage)
+        self.pixmap = self.pixmap.scaled(QSize(int(self.parent.size().width() / 2), int(self.parent.size().height() / 2)), Qt.KeepAspectRatio)
         self.scene = QGraphicsScene(self)
-        self.image_item = QGraphicsPixmapItem(pixm)
+        self.image_item = QGraphicsPixmapItem(self.pixmap)
+        #  QGraphicsPixmapItem::setOpacity
         self.scene.addItem(self.image_item)
         pen = QPen()
         pen.setColor(QColor(0, 0, 255))
-        self.line1 = self.scene.addLine(350, 0, 350, 400, pen)
-        self.line2 = self.scene.addLine(0, 300, 400, 300, pen)
-        # self.setStyleSheet("QGraphicsView{background-color:rgb(0,0,0);}")
+        #self.line1 = self.scene.addLine(350, 0, 350, 400, pen)
+        self.line1 = self.scene.addLine(int(self.pixmap.size().width()/2), 0, int(self.pixmap.size().width()/2), self.pixmap.size().height(), pen)
+        self.line2 = self.scene.addLine(0, int(self.pixmap.size().height()/2), self.pixmap.size().width(), int(self.pixmap.size().height()/2), pen)
+        self.setStyleSheet("QGraphicsView{background-color:rgb(0,0,0);}")
         self.setScene(self.scene)
+        self.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+        self.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
+
+        self.width_diff = int(self.parent.size().width() / 2) - self.pixmap.width()
+        self.height_diff = int(self.parent.size().height() / 2) - self.pixmap.height()
         # self.initializeGL()
         # self.paintGL()
 
@@ -58,6 +67,19 @@ class CustomQOpenGLWidget(QGraphicsView):
     def mousePressEvent(self, event):
         if event.button() == Qt.LeftButton:
             print("Position: {}".format(event.localPos()))
-            actual_x = max(event.localPos().x()-int(145/2), int(145/2))
-            self.line1.setLine(actual_x, 0, actual_x, 400)
-            self.line2.setLine(0, event.localPos().y(), 350, event.localPos().y())
+            actual_x = event.localPos().x() - int(self.width_diff / 2)
+            if event.localPos().x() - int(self.width_diff / 2) < 0:
+                actual_x = 0
+            elif event.localPos().x() >= self.pixmap.size().width() + int(self.width_diff / 2):
+                actual_x = self.pixmap.size().width() # + int(self.width_diff / 2)
+            self.line1.setLine(actual_x, 0, actual_x, self.pixmap.size().height())
+
+            actual_y = event.localPos().y() - int(self.height_diff / 2)
+            if event.localPos().y() - int(self.height_diff / 2) < 0:
+                actual_y = 0
+            elif event.localPos().y() >= self.pixmap.size().height() + int(self.height_diff / 2):
+                actual_y = self.pixmap.size().height() #+ int(self.height_diff / 2)
+
+            self.line2.setLine(0, actual_y, self.pixmap.size().width(), actual_y)
+
+            self.coordinates_changed.emit(actual_x, actual_y)
