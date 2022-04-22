7e7fe557b5d44417059c96a7c255e57de2074afb
dbouget <bougetd@gmail.com>
2022-04-22 14:23:29 +0200

    Andre....

--- gui2/SinglePatientComponent/SinglePatientWidget.py
+++ gui2/SinglePatientComponent/SinglePatientWidget.py
@@ -131,6 +131,7 @@ class SinglePatientWidget(QWidget):
         self.import_data_dialog.mri_volume_imported.connect(self.layers_panel.on_mri_volume_import)
         self.import_data_dialog.annotation_volume_imported.connect(self.layers_panel.on_annotation_volume_import)
         self.import_data_dialog.patient_imported.connect(self.results_panel.on_import_patient)
+        #TODO
 
         self.import_data_triggered.connect(self.center_panel.on_import_data)
         self.import_data_triggered.connect(self.results_panel.on_import_data)
