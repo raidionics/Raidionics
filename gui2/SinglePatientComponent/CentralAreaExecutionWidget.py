b437e1fef076164c297b4806f60881b50cb03f31
andreped <andrped94@gmail.com>
2022-05-12 21:59:11 +0200

    renaming of backend binaries, and removed tmp dir deletion for macos

--- gui2/SinglePatientComponent/CentralAreaExecutionWidget.py
+++ gui2/SinglePatientComponent/CentralAreaExecutionWidget.py
@@ -112,7 +112,7 @@ class CentralAreaExecutionWidget(QWidget):
             with open(seg_config_filename, 'w') as outfile:
                 seg_config.write(outfile)
 
-            subprocess.call(['raidionicsseg', seg_config_filename])
+            subprocess.call(['raidionicsseg_bin', seg_config_filename])
 
             seg_file = os.path.join(current_patient_parameters.output_folder, 'labels_Tumor.nii.gz')
             shutil.move(seg_file, os.path.join(current_patient_parameters.output_folder, 'patient_tumor.nii.gz'))
@@ -236,7 +236,7 @@ class CentralAreaExecutionWidget(QWidget):
             with open(rads_config_filename, 'w') as outfile:
                 rads_config.write(outfile)
 
-            subprocess.call(['raidionicsrads', rads_config_filename])
+            subprocess.call(['raidionicsrads_bin', rads_config_filename])
             self.__collect_reporting_outputs(current_patient_parameters)
         except Exception:
             print('{}'.format(traceback.format_exc()))
--- main_bundle.spec
+++ main_bundle.spec
@@ -19,8 +19,6 @@ hidden_imports = [x.split("=")[0] for x in hidden_imports] + ["medpy", "ants", "
 hidden_imports = [x.lower() for x in hidden_imports]
 
 # copy dependencies and overwrite if already exists (as well as images)
-if os.path.exists("./tmp_dependencies/"):
-    shutil.rmtree("./tmp_dependencies/")
 shutil.copytree("./images/", "./tmp_dependencies/images/")
 shutil.copytree("./utils/", "./tmp_dependencies/utils/")
 shutil.copytree("./gui2/", "./tmp_dependencies/gui2/")
