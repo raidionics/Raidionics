5db466c6e0feff6a270fc6d442ab43bf9e97f255
dbouget <bougetd@gmail.com>
2023-08-24 16:29:41 +0200

    Removing unused hooks

--- assets/hooks/hook-tensorflow.py
+++
@@ -1,6 +0,0 @@
-from PyInstaller.utils.hooks import collect_submodules
-from PyInstaller.utils.hooks import collect_data_files
-
-hiddenimports = collect_submodules("tensorflow")
-
-datas = collect_data_files("tensorflow")
