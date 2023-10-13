c14657db668d063a38157fa19d70252141dd360a
dbouget <bougetd@gmail.com>
2023-10-13 09:33:06 +0200

    Adjusting Ubuntu CI (and opencv package) for running the test GUI [skip ci]

--- tests/software_launch_test.py
+++ tests/software_launch_test.py
@@ -23,20 +23,21 @@ def software_launch_test():
             proc = subprocess.Popen([os.path.join(build_executable_path, 'Raidionics')], stdout=subprocess.PIPE,
                                     stderr=subprocess.PIPE, shell=True,
                                     creationflags=subprocess.CREATE_NEW_PROCESS_GROUP)
-            stdout, stderr = proc.communicate()
             time.sleep(10)
+            stderr = proc.stderr
             proc.send_signal(signal.CTRL_BREAK_EVENT)
             proc.kill()
         else:
             proc = subprocess.Popen([os.path.join(build_executable_path, 'Raidionics')], stdout=subprocess.PIPE,
-                                    stderr=subprocess.PIPE, shell=True, preexec_fn=os.setsid)
-            # stdout, stderr = proc.communicate()
+                                    stderr=subprocess.PIPE, preexec_fn=os.setsid)
             time.sleep(10)
+            stderr = proc.stderr
             os.killpg(os.getpgid(proc.pid), signal.SIGTERM)
 
-        # # @TODO. Should likely parse the stdout or stderr content to identify any potential issue!
-        # if 'error' in stderr.decode("utf-8").lower() or 'failed' in stderr.decode("utf-8").lower():
-        #     raise ValueError("Error during software launch unit test with.\n")
+        # @TODO. Should likely parse the stdout or stderr content to identify any potential issue!
+        if stderr is not None:
+            if 'error' in stderr.decode("utf-8").lower() or 'failed' in stderr.decode("utf-8").lower():
+                raise ValueError("Error during software launch unit test with.\n")
     except Exception as e:
         logging.error("Error during software launch unit test with: \n {}.\n".format(traceback.format_exc()))
         raise ValueError("Error during software launch unit test with.\n")
