253e9beb082a8318958b236a95fbf955b9955d03
dbouget <bougetd@gmail.com>
2023-10-20 13:51:29 +0200

    Seemingly working modified rtutils backend to generate DICOM RTStruct [skip ci]

--- assets/hooks/hook-rt-utils.py
+++
@@ -1,6 +0,0 @@
-from PyInstaller.utils.hooks import collect_submodules
-from PyInstaller.utils.hooks import collect_data_files
-
-hiddenimports = collect_submodules("rt-utils")
-
-datas = collect_data_files("rt-utils")
