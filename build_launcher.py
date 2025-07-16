95bcf73d331da9ae3a504fc6d8a6636da030131d
dbouget <bougetd@gmail.com>
2025-07-16 11:20:07 +0200

    CI issues to fix[skip ci]

--- build_launcher.py
+++ build_launcher.py
@@ -1,20 +1,13 @@
 import sys
+import PyInstaller.__main__
+
+# Set recursion limit early
 sys.setrecursionlimit(5000)
+print("Recursion limit before build:", sys.getrecursionlimit())
 
-import PyInstaller.building.utils
-original_exec_statement = PyInstaller.building.utils.exec_statement
-
-def exec_statement_with_recursion_fix(statement, *args, **kwargs):
-    fixed_statement = f"import sys; sys.setrecursionlimit(5000); {statement}"
-    return original_exec_statement(fixed_statement, *args, **kwargs)
-
-PyInstaller.building.utils.exec_statement = exec_statement_with_recursion_fix
-
-# Now run PyInstaller
-from PyInstaller.__main__ import run
-run([
+PyInstaller.__main__.run([
     '--log-level=INFO',
     '--noconfirm',
     '--clean',
-    'assets/main_arm.spec',
+    'assets/main_arm.spec'
 ])
